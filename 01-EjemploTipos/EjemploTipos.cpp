9c22bd70084b4c25a5f064d6f816de4d6d4e2ff4
Julian Luna <julian.lunaarecha@web.com>
2022-05-19 21:17:21 -0300

    Ejercicio 1 - Agrego assert con string_literals

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -12,6 +12,8 @@
                     // (asegurar, afirmar, aseverar o asertar) de C++.
                     // El assert asevera si la sentencia dentro de los parentesis es verdadera.
 
+using namespace std::string_literals;
+
 int main() {    // Define la clase principal del programa por donde C++ ingresa, siempre retorna un entero que se define como el estado de la ejecucion.
 
     assert(true);                   // Aseveramos si la constante booleana es verdadera.
@@ -19,5 +21,6 @@ int main() {    // Define la clase principal del programa por donde C++ ingresa,
     assert(666 == 666);             // Aseveramos si la constante entera sin signo es igual a 666.
     assert(3.14159 > -7);           // Aseveramos si la constante de punto flotante con doble precision es mayor a la constante entera.
     assert(12 == std::string("Las Palabras").length()); // Aseveramos si la constante cadena de caracteres tiene 12 caracteres de largo.
+    assert(12 == "Las Palabras"s.length()); // Aseveramos si la constante cadena de caracteres utilizando string_literals tiene 12 caracteres de largo.
 
 }   // Cerramos la clase principal main.
