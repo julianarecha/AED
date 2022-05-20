e0fe10fbe8258bb7d200bfcb06e99d7d60139177
Julian Luna <julian.lunaarecha@web.com>
2022-05-19 21:43:19 -0300

    Ejercicio 1

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -20,8 +20,17 @@ int main() {    // Define la clase principal del programa por donde C++ ingresa,
     assert('a' != 'b');             // Aseveramos si la constante caracter no es b.
     assert(666 == 666);             // Aseveramos si la constante entera sin signo es igual a 666.
     assert(3.14159 > -7);           // Aseveramos si la constante de punto flotante con doble precision es mayor a la constante entera.
+    assert(3.14159 > 3e-2); // Notacion cientifica
     assert(12 == std::string("Las Palabras").length()); // Aseveramos si la constante cadena de caracteres tiene 12 caracteres de largo.
     assert(12 == "Las Palabras"s.length()); // Aseveramos si la constante cadena de caracteres utilizando string_literals tiene 12 caracteres de largo.
-    assert("abcd"s == "ab"s + "cd"s);
+    assert("abcd"s == "ab"s + "cd"s); // Concatenacion de cadenas de caracteres
+    assert(666u == 666u); // Unsigned
+    assert(667l >= 666l); // Long
+    assert(2 == 2u);
+    assert(1); // TODO Limites de cada tipo de dato
+    assert(sizeof 'a' == 1); // Memoria de cada tipo de dato
+    assert(sizeof 65 == 4); // Memoria de cada tipo de dato
+    assert(sizeof "Hola"s == 32); // Memoria de cada tipo de dato
+    assert(sizeof "Hola" == 5); // Memoria de cada tipo de dato
 
 }   // Cerramos la clase principal main.
