b5e4f7ff7568b8dca801e041461b0a7ec551cb4b
Julian Ismael Luna  Arecha <flashhammer@gmail.com>
2022-06-02 20:59:36 -0300

    TP0

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -6,7 +6,23 @@
 
 #include <iostream>
 
+using namespace std::string_literals;
+
 int main() {
-    std::cout << "Hello, World!\n";
-    return 0;
+//    std::cout << "Hola Mundo\n";
+    std::cout << "Hola Mundo\n"s;
+//    std::cout << "Hola"s + " "s + "Mundo"s + "\n"s;
+//    std::cout << ("Hola "s + "Mundo\n"s);
+//    std::cout << "Hola "s + "Mundo\n"s;
+//    //(std::cout << "Hola "s) + "Mundo\n"s;
+//    (std::cout << "Hola "s) << "Mundo\n"s;
+//    std::cout << "Hola "s;
+//    std::cout << "Mundo\n"s;
+//    std::cout << "Hola "s << "Mundo\n"s;
+//    std::cout << 1 + 2 + 3;
+//    std::cout << true + false + true;
+//    std::cout << 1l + 2l + 3l;
+//    std::cout << 10 / 3 ;
+//    std::cout << "\n"s;
+//    std::cout << 10.0 / 3.0 ;
 }
